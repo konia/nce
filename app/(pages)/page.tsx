0fc6ed74420e576c7d6e173dd2fd2e4db4f85bab
Qi Hua Huo <huoqihua@cn.ibm.com>
2025-12-31 13:24:05 +0800

    update

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function HomePage() {
+  return (
+    <>
+      <section className="mb-12">
+        <Button>Button</Button>
+      </section>
+    </>
+  );
+}
